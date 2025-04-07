f5fd2e1020580bd003a4e4e5d8069f1b44c35d71
silaswpo <silaswesleypereiradeoliveira@gmail.com>
2025-04-07 14:51:44 -0300

    Validando

--- operar_vetores.v
+++ operar_vetores.v
@@ -1,9 +1,18 @@
-module operar_vetores( 
+module operar_vetores(
     input [2:0] a,
     input [2:0] b,
     output [2:0] saida_or_bit_a_bit,
     output saida_or_logico,
     output [5:0] saida_not
 );
-	//insira seu código aqui
+
+// OR bit a bit (bitwise)
+assign saida_or_bit_a_bit = a | b;
+
+// OR lógico (1 se qualquer bit de a ou b for 1)
+assign saida_or_logico = |a | |b;
+
+// NOT bit a bit da concatenação de b (MSB) e a (LSB)
+assign saida_not = ~( {b, a} );
+
 endmodule
